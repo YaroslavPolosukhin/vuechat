ed05e28f573032cffd625fc70c7d12cba8d13f4d
YaroslavPolosukhin <polosukhin.yaroslav@gmail.com>
2023-12-17 12:41:16 +0300

    worked components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
-<script setup>
+<template>
+  <ContainerElement>
+    <ChatWindow>
+      <ChatMessage username="Ivan" datetime="21.12.2019 05:00:50">Hello, World!!</ChatMessage>
+    </ChatWindow>
+  </ContainerElement>
+</template>
+
+<script>
+  import {defineComponent} from "vue";
+  import ContainerElement from "@/components/Container.vue";
+  import ChatWindow from "@/components/ChatWindow.vue";
+  import ChatMessage from "@/components/ChatMessage.vue";
+
+  export default defineComponent({
+    components: {ChatMessage, ChatWindow, ContainerElement}
+  })
 
 </script>
 
-<template>
-
-</template>
-
-<style scoped>
-
+<style>
+  body {
+    margin: 0;
+    background-color: #f9f9fa;
+  }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "containerElement"
+}
+</script>
+
+<style scoped>
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+</style>
